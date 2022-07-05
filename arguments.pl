b10e77c072b0ec26b3e04c6a569817a50d86e33e
ZWC <zealotwithcharge@gmail.com>
2022-07-05 14:22:27 -0700

    restructured functors in the form formula(operator,inner_formula1,inner_formula2,justification,line)

--- arguments.pl
+++ arguments.pl
@@ -1,8 +1,7 @@
 
 
-arrow(a_formula(test,test,_),universal(possible,_)).
-a_formula(possible,unicorns,[premise]).
-universal(necessary,[premise]).
-a_formula(u_flag_test,u_flag,[premise]).
-
+formula(arrow,formula(unit,test,test,_),formula(universal,possible,_),[premise1],1).
+formula(unit,possible,unicorns,[premise2],1).
+formula(universal,necessary,[premise3],1).
+formula(unit,u_flag_test,u_flag,[premise4],1).
 
