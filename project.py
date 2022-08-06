da1cfb731d65e84a37cf8161aeef03ead4bf9433
ImMoNE <chwang22@cmc.edu>
2022-08-06 05:39:26 -0700

    final backup for ubuntu

--- project.py
+++ project.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python.exe
 
-
 ########################################
 # flask/db setup
 ########################################
 
 from flask import Flask, render_template, request, make_response, redirect, url_for
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
-
+cors = CORS(app)
+#,resources={r"/": {"origins": "http://localhost:5000"}})
 # sqlite3 is built in python3, no need to pip3 install
 import random
 import sqlite3
 import copy
+import logging
 # process command line arguments
 
+logging.getLogger('flask_cors').level = logging.DEBUG
 
 @app.route('/',methods = ['GET','POST'])     
+@cross_origin()
 def root():
     con = sqlite3.connect('sql_ex.db')
     cur = con.cursor()
@@ -35,9 +39,10 @@ def root():
         split = row[0].split('-')
         temp = [copy.deepcopy(split[0]),copy.deepcopy(split[1])]
         links.append(temp)
+    response = make_response(render_template('index.html',raw_nodes = nodes, raw_links = links))
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
-    
-    return render_template('index.html',raw_nodes = nodes, raw_links = links)
 ########################################
 # boilerplate
 ########################################
